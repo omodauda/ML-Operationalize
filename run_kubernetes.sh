b5d11d9460f6a091a0590ce9c47dd25a2b487df3
omodauda <omodauda.dl@gmail.com>
2022-06-20 22:36:50 +0100

    add

---
+++ run_kubernetes.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+# dockerpath=<>
+dockerpath=omodauda/machine-learning-api
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run machine-learning-api --image=$dockerpath
+
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward machine-learning-api 8080:80
+
